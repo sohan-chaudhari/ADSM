b29f59e8e7d4e140ca68dc195f2e5b8cc6d458ac
sohan-chaudhari <sohanchaudhari384240@gmail.com>
2025-01-18 11:50:49 +0530

    Add files via upload

---
+++ ADSM.py
@@ -0,0 +1,7 @@
+x=int(input("enter the number :"))
+y=int(input("enter the anthor number :"))
+
+print("add two numbers :",x+y)
+print("devide two number:",x/y)
+print("subtrat two number :",x-y)
+print("multiply two number :",x*y)
