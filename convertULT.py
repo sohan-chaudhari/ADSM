509a04621c63d6aa222986b8fa119a3543657f80
sohan-chaudhari <sohanchaudhari384240@gmail.com>
2025-01-21 11:53:02 +0530

    Add files via upload

---
+++ convertULT.py
@@ -0,0 +1,7 @@
+def convert(str):
+    print("convert into lowercase:",str.lowercase())
+    print("convert into uper: ",str.upper())
+    print("convert into toggles :",str.toggle())
+
+str=input("enter the string :")
+convert(str)
